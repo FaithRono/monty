19dc8ad87fa282c7ec814ee84d85a59d8e34b748
Owen Gitau <owengitau02@gmail.com>
2023-11-16 05:59:28 -0800

    monty

--- stack_ops.c
+++ stack_ops.c
@@ -110,3 +110,20 @@ void nop(stack_t **stack, unsigned int line)
         (void)stack;
         (void)line;
 }
+
+/**
+ * pint - prints the value at the top of the stack, followed by a new line.
+ * @stack: the stack
+ * @line_number: the current line number
+ *
+ * Return: void
+ */
+void pint(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		fprintf(stdout, "L%d: can't pint, stack empty\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", (*stack)->n);
+}
